c452938cfe4e09909ada52710a832045651d33d1
A Harvey Pfoccori Quispe <105808965+Aleticod@users.noreply.github.com>
2022-12-16 05:00:14 +0100

    Change python to python3

--- run-project.sh
+++ run-project.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 imageName=$1
 kernelType=$2
-python convert-image-to-txt.py "${imageName}.jpg" $kernelType
+python3 convert-image-to-txt.py "${imageName}.jpg" $kernelType
 g++ process-image.cpp -o process-image.exe
 ./process-image.exe ./results/txt_$imageName.txt ./results/data_$imageName.txt $kernelType
 convert ./results/${imageName}_result.pgm ./results/${imageName}_result.jpg
