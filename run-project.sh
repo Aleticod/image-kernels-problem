cf7d504d6d44789da1509e2703d9118d9e10bf62
A Harvey Pfoccori Quispe <105808965+Aleticod@users.noreply.github.com>
2022-12-30 09:35:55 -0500

    :alien: Add -fopenmp to compile cpp script

--- run-project.sh
+++ run-project.sh
@@ -3,6 +3,6 @@ imageName=$1
 kernelType=$2
 threadNum=$3
 python3 convert-image-to-txt.py "${imageName}.jpg" $kernelType $threadNum
-g++ process-image.cpp -o process-image.exe
+g++ -fopenmp process-image.cpp -o process-image.exe
 ./process-image.exe ./results/txt_$imageName.txt ./results/data_$imageName.txt
 convert ./results/${imageName}_result.pgm ./results/${imageName}_result.jpg
